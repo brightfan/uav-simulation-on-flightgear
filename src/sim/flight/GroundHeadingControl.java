739938d24b8ff68ac53686d3ecf97502050e0a0e
brightfan.fy <brightfan.fy@dd8f95be-0cf6-11df-9821-6b6fb8eff287>
2010-02-26 03:35:00 +0000

    Added PID control to taking off (successful) Added PID control to roll stabilization (unsuccessful, severe oscillation)

---
+++ src/sim/flight/GroundHeadingControl.java
@@ -0,0 +1,46 @@
+package sim.flight;
+
+public class GroundHeadingControl extends PIDBase{
+	
+	public GroundHeadingControl(double KP, double KI, double KD) {
+		super(KP, KI, KD);
+	}
+
+	private double processError(double error) {
+		if(error > 0) {
+			if(error > 180) {
+				error = error - 360;
+			} else {
+				; // do nothing
+			}
+		} else {
+			if (error < -180) {
+				error = error + 360;
+			} else {
+				; //do nothing
+			}
+		}
+		
+		if (Math.abs(error) > 3) {
+			return error;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	@Override
+	public double getResult(double error) {
+		double trimmedError = processError(error);
+		
+		accumulateError(trimmedError);
+		
+		//System.out.println("P is : " + error);
+		//System.out.println("I is : " + accumulatedError);
+		//System.out.println("D is : " + trimmedError);
+		
+		double result =  KP * error + KI * accumulatedError + KD * (currentError - previousError);
+		
+		return result/5;
+	}
+}
