ce4f18d6b071b31be010f2ea611b0d1945d47a09
brightfan.fy <brightfan.fy@dd8f95be-0cf6-11df-9821-6b6fb8eff287>
2010-03-06 03:06:21 +0000

    Basic Straight Line Flight Achieved Rudder is controlled by P I D Elevator and Aileron are controlled by Sin(P) Throttle is fixed to 70%

---
+++ trunk/src/sim/flight/YawDegsControl.java
@@ -0,0 +1,38 @@
+package sim.flight;
+
+public class YawDegsControl extends PIDBase {
+
+	public YawDegsControl(double KP, double KI, double KD) {
+		super(KP, KI, KD);
+	}
+
+	@Override
+	public double getResult(double error) {
+		
+		//double trimmedError = processError(error);
+		//accumulateError(trimmedError);
+		accumulateError(error);
+		
+		double result =  KP * error + KI * accumulatedError + KD * (currentError - previousError);
+		//double result = KP * error;
+		//double result = Math.sin(Math.toRadians(error));
+		
+		//result *= 2.0;
+		System.out.println("P is : " + error);
+		System.out.println("I is : " + accumulatedError);
+		System.out.println("D is : " + (currentError - previousError));
+		System.out.println("result is : " + result + " ********");
+		
+		if (result > 0.5) {
+			result = 0.5;
+		}
+		else if (result < -0.6) {
+			result = - 0.6;
+		}
+		
+		if (Math.abs(error) > 2.0)
+			return result;
+		else
+			return 0;
+	}
+}
