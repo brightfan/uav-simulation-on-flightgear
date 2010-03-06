6978bce60c466418639162d02127084444cf71be
brightfan.fy <brightfan.fy@dd8f95be-0cf6-11df-9821-6b6fb8eff287>
2010-03-06 23:47:36 +0000

    Redefined ID of Waypoint class

--- trunk/src/sim/airport/KSFO.java
+++ trunk/src/sim/airport/KSFO.java
@@ -6,7 +6,7 @@ public class KSFO extends Airport {
 	public KSFO() {
 		this.runway = "1L";
 		this.setDirection(298.0);
-		this.setLandingPoint(new Waypoint(37.61354101, -122.3572386));
-		this.setTakingoffPoint(new Waypoint(37.62863534, -122.3929095));
+		this.setLandingPoint(new Waypoint("Airport", 37.61354101, -122.3572386));
+		this.setTakingoffPoint(new Waypoint("Airport", 37.62863534, -122.3929095));
 	}
 }
