7b535cbd90a9dd5b28f72bac81de17fd646be2fd
brightfan.fy <brightfan.fy@dd8f95be-0cf6-11df-9821-6b6fb8eff287>
2010-03-08 05:39:22 +0000

    Optimize the KMLFileWritter. Use FileOutputStream to substitute KML API.

--- trunk/src/sim/utils/KMLFileWritter.java
+++ trunk/src/sim/utils/KMLFileWritter.java
@@ -8,8 +8,7 @@ public class KMLFileWritter {
 	public static String filePath = "/Users/Helicopter/Software/apache-tomcat-6.0.20/webapps/kml/test.kml";
 	public static FileOutputStream fOutStream;
 
-	public static void writeToFile(float latitude, float longitude,
-			float altitude, float heading, float tilt) {
+	public static void writeToFile(float latitude, float longitude) {
 
 		//Kml kml = new Kml();
 		
@@ -23,13 +22,25 @@ public class KMLFileWritter {
 		//		latitude);
 		
 		String kmlStr = "<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:xal=\"urn:oasis:names:tc:ciq:xsdschema:xAL:2.0\">" +
-		"<Placemark>" +
-		"<name>current position</name>" +
+		"<Document>" +
+		"<name>UAV Tracking</name>" +
 		"<open>1</open>" +
+		"<Style id=\"highlightPlacemark\">" +
+		"<IconStyle>" +
+		//"<color>641400FF</color>" +
+		"<Icon>" +
+        "<href>http://maps.google.com/mapfiles/ms/icons/plane.png</href>" +
+        "</Icon>" +
+		"</IconStyle>" +
+		"</Style>" +
+		"<Placemark>" +
+		"<name>C-172N</name>" +
+		"<styleUrl>#highlightPlacemark</styleUrl>" +
 		"<Point>" +
 		"<coordinates>"+longitude+","+latitude+"</coordinates>" +
 		"</Point>" +
 		"</Placemark>" +
+		"</Document>" +
 		"</kml>";
 	
 		try {
@@ -49,15 +60,15 @@ public class KMLFileWritter {
 
 		double lat1 = 37.61354101;
 		double lon1 = -122.3572386;
+		int i = 0;
 		
 		while (true) {
-			writeToFile((float) lat1, (float) lon1, (float) 100, (float) 298,
-					(float) 90);
+			writeToFile((float) lat1, (float) lon1);
 			Waypoint wp = GlideSlope.generateWaypointByDistanceAndBearing(lat1,
 					lon1, 30, 298);
 			lat1 = wp.getLatitude();
 			lon1 = wp.getLongitude();
-			System.out.println("*******");
+			System.out.println(i++);
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
