ca28117ed4b9d6c8820635af41a1b320162c2668
brightfan.fy <brightfan.fy@dd8f95be-0cf6-11df-9821-6b6fb8eff287>
2010-04-23 23:03:20 +0000

    changed the constraint on PitchDegreeControls

--- src/sim/controller/PitchDegsControl.java
+++ src/sim/controller/PitchDegsControl.java
@@ -7,29 +7,28 @@ public class PitchDegsControl extends PIDBase{
 		super(KP, KI, KD);
 	}
 	
-	@Override
-	public double getResult(double error) {
+	public double getResult(double error, float derivation) {
 		
 		//double trimmedError = processError(error);
 		//accumulateError(trimmedError);
 		accumulateError(error);
 		
-		double result =  KP * error;// + KI * accumulatedError;
+		double result =  KP * error + KI * accumulatedError + KD * derivation;
 		//double result = KP * error;
 		
 		//System.out.println("P is : " + error);
 		//System.out.println("I is : " + accumulatedError);
 		//System.out.println("result is : " + result + " ********");
 		
-		if (result > 0.5) {
-			result = 0.5;
+		if (result > 0.4) {
+			result = 0.4;
 		}
-		else if (result < -0.5) {
-			result = - 0.5;
+		else if (result < -0.4) {
+			result = - 0.4;
 		}
 		
 		if (Math.abs(error) > 0.05)
-			return - result;
+			return result;
 		else
 			return 0;
 	}
