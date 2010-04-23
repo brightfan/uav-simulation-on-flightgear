c7828971ae5af60acdfe92fcfc06a61d284578d7
brightfan.fy <brightfan.fy@dd8f95be-0cf6-11df-9821-6b6fb8eff287>
2010-04-23 04:42:30 +0000

    Height control and Direction control for navigation are basically done.

--- trunk/src/sim/flight/Navigation.java
+++ trunk/src/sim/flight/Navigation.java
@@ -2,6 +2,7 @@ package sim.flight;
 
 import sim.aircraft.Aeroplane;
 import sim.airport.Airport;
+import sim.controller.AirborneSpeedControl;
 import sim.controller.PitchDegsControl;
 import sim.controller.RollDegsControl;
 import sim.controller.YawDegsControl;
@@ -11,16 +12,20 @@ public class Navigation extends AutoPilot {
 
 	/* flight controller */
 	private RollDegsControl rollDegsControl;
-	private YawDegsControl yawDegsControl;
+	//private YawDegsControl yawDegsControl;
 	private PitchDegsControl pitchDegsControl;
+	private AirborneSpeedControl airborneSpeedControl;
 
 	private double navigationHeight = 600;
 
 	public Navigation(Aeroplane aeroplane, Airport airport) {
+
 		super(aeroplane, airport);
-		rollDegsControl = new RollDegsControl(0.5, 0.0002, 0.0);
-		yawDegsControl = new YawDegsControl(0.06, 0.0000, 0.0);
-		pitchDegsControl = new PitchDegsControl(0.25, 0.0, 0.0);
+
+		rollDegsControl = new RollDegsControl(0.0333, 0.0002, 0.0);
+		//yawDegsControl = new YawDegsControl(0.06, 0.0000, 0.0);
+		pitchDegsControl = new PitchDegsControl(0.1, 0.005, 0.01);
+		airborneSpeedControl = new AirborneSpeedControl(0.03, 0.0001, 0.0);
 
 		System.out.println("********** Entered Navigation Mode! ***********");
 	}
@@ -29,40 +34,67 @@ public class Navigation extends AutoPilot {
 	public void autoPilot() {
 		while (true) {
 			aeroplane.readStatus();
-			
+
 			double currentHeight = aeroplane.getAltitudeFt();
 			double desiredPitch;
-			
-			desiredPitch = (navigationHeight - currentHeight) / 500 * 2;
 
-			throttle = (float) 0.55;
+			desiredPitch = (1 / (Math.pow(1.05, -(navigationHeight - currentHeight)) + 1) - 0.5) * 60;
 
-			// System.out.println("Heading error is: "
-			// + (airport.getRunwayDirection() -
-			// aeroplane.getHeadingDeg()));
-			rudder = (float) yawDegsControl.getResult(airport
+			// throttle = (float) 0.55;
+			throttle = (float) airborneSpeedControl.getResult(100 - aeroplane
+					.getGroundSpeedKt());
+
+			/*rudder = (float) yawDegsControl.getResult(airport
 					.getRunwayDirection()
-					- aeroplane.getHeadingDeg());
-			// System.out.println("Rolling error is: "
-			// + (0 - aeroplane.getRollDeg()));
-			aileron = (float) rollDegsControl.getResult(0 - aeroplane
+					- aeroplane.getHeadingDeg());*/   /* THIS IS WORKING!!!! */
+		
+			double directionError = airport.getRunwayDirection()
+									- aeroplane.getHeadingDeg();
+			if (directionError < 0) {
+				if (directionError < -180) {
+					directionError += 360;
+				}
+			} else {
+				if (directionError > 180) {
+					directionError -= 360;
+				}
+			}
+			
+			if (directionError > 90) {
+				directionError = 90;
+			}
+			else if (directionError < -90) {
+				directionError = -90;
+			}
+				
+			double desiredAileron = Math.sin(Math.toRadians(directionError)) * (20);
+			
+			aileron = (float) rollDegsControl.getResult(desiredAileron - aeroplane
 					.getRollDeg());
-			// System.out.println("Pitch Degree is: "
-			// + aeroplane.getPitchDeg());
-			elevator = (float) pitchDegsControl.getResult(desiredPitch);
+			
+			elevator = (float) pitchDegsControl.getResult(aeroplane
+					.getPitchDeg()
+					- desiredPitch, aeroplane.getPitchRateDegps());
 
-			rudder += (float) 0.0075;
+			//rudder += (float) 0.0075;
+			rudder = (float) 0;
 			aileron += (float) 0.006;
 			brakeParking = 1;
 
-			System.out
-					.println("current speed is: " + aeroplane.getAirSpeedKt());
-			System.out.println("current height is: "
-					+ aeroplane.getAltitudeFt());
-			System.out.println("current elevator is: " + elevator);
-			System.out.println("current pitch is: " + aeroplane.getPitchDeg());
-			System.out.println("desired pitch is: " + desiredPitch);
-
+			logToCommandLine();
+			
+			if (commandlineLogCounter == ONESECONDCOUNTER) {
+				/* FOR AILERON DEBUG */
+				/*System.out.println("Direction Error: " + directionError);
+				System.out.println("DesiredAileron: " + desiredAileron);
+				System.out.println("Current roll degree is: " + aeroplane.getRollDeg());*/
+				
+				/* FOR ELEVATOR DEBUG */
+				System.out.println("Height Error: " + (navigationHeight - currentHeight));
+				System.out.println("Desired Pitch: " + desiredPitch);
+				System.out.println("Elevator: " + elevator);
+			}
+			
 			if (isLogging) {
 				writeCount++;
 				if (writeCount > 15) {
@@ -76,5 +108,4 @@ public class Navigation extends AutoPilot {
 			sendCommand();
 		}
 	}
-
 }
