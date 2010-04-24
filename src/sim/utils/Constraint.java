0fa5b285388088795902affe09d1e5d1ffaeec9e
brightfan.fy <brightfan.fy@dd8f95be-0cf6-11df-9821-6b6fb8eff287>
2010-04-24 21:30:15 +0000

    better name

--- src/sim/utils/Constraint.java
+++ src/sim/utils/Constraint.java
@@ -1,12 +1,12 @@
 package sim.utils;
 
 public class Constraint {
-	public static double constraint(double value, double upperLimit, double bottomLimit) {
+	public static double constraint(double value, double upperLimit, double lowerLimit) {
 		if (value > upperLimit) {
 			return upperLimit;
 		}
-		else if (value < bottomLimit) {
-			return bottomLimit;
+		else if (value < lowerLimit) {
+			return lowerLimit;
 		}
 		else {
 			return value;
